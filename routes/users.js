47c3231ce8bc2274d427b8717344f5196f5754fd
MostaphaEchelhi <mostapha.echelhi@medieinstitutet.se>
2023-03-06 19:12:30 +0100

    update

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 let users = [
-  {id: 1, name: "Mostapha"},
-  {id: 2, name: "Bob"},
-  {id: 3, name: "Ahmed"},
-  {id: 4, name: "johan"},
-  {id: 5, name: "Mohammed"}
+  {id: 1, name: "Mostapha", password: "fmp123"},
+  {id: 2, name: "Bob", password: "sth145"},
+  {id: 3, name: "Ahmed", password: "momo"},
+  {id: 4, name: "johan", password: "Sverige"},
+  {id: 5, name: "Mohammed", password: "Marocko"}
 ]
 
 /* GET users listing. */
@@ -25,4 +25,21 @@ router.post('/', function(req, res, next) {
   res.json(users);
 });
 
+router.post('/login', function(req, res, next) {
+  const {name, password} = req.body;
+  const foundUser = users.find(user => user.name === name);
+
+  if (password === foundUser.password) {
+    res.status(201).json({name: foundUser.name, id:foundUser.id})
+  } else {
+    res.status(401).json("OPSS")
+  }
+});
+
 module.exports = router;
+
+
+
+
+
+
