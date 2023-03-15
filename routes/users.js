437fa047c5998646c98c970954c7d498e23f7eae
MostaphaEchelhi <mostapha.echelhi@medieinstitutet.se>
2023-03-15 22:56:17 +0100

    update

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
+const { json } = require('express');
 var express = require('express');
 var router = express.Router();
+const fs = require("fs");
+const crypto = require("crypto-js");
+// const salt = "pig are shit";
 
 let users = [
   {id: 1, name: "Mostapha", password: "fmp123"},
@@ -11,7 +15,15 @@ let users = [
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json(users);
+  //res.json(users);
+  fs.readFile("users.json", function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(data)
+      return;
+    }
+  })
 });
 
 router.get('/:userId', function(req, res, next) {
@@ -25,25 +37,48 @@ router.get('/:userId', function(req, res, next) {
 
 
 router.post('/', function(req, res, next) {
+  let newUser = {name: req.body.name};
+  fs.readFile("users.json", function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      let users = JSON.parse(data)
+      newUser.id = users.length +1;
+      let passwordToSave = crypto.SHA3(req.body.password).toString();
+      //let passwordToSave = crypto.AES.encrypt(req.body.password, salt).toString();
+      newUser.password = passwordToSave;
+      users.push(newUser)
+      fs.writeFile("users.json", JSON.stringify(users, null, 2), function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json(users);
+        }
 
-  let newUser = req.body;
-  newUser.id = users.length +1;
-  users.push(newUser);
-  console.log(newUser);
-
-
-  res.json(users);
+      })
+    }
+  })
 });
 
 router.post('/login', function(req, res, next) {
   const {name, password} = req.body;
-  const foundUser = users.find(user => user.name === name);
-
-  if (password === foundUser.password) {
-    res.status(201).json({name: foundUser.name, id:foundUser.id})
-  } else {
-    res.status(401).json("OPSS")
-  }
+  fs.readFile("users.json", function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      let users = JSON.parse(data)
+      const foundUser = users.find(user => user.name === name);
+      if (crypto.SHA3 (password).toString() === foundUser.password) {
+      //if (password === crypto.AES.decrypt(foundUser.password, salt).toString(crypto.enc.Utf8)) {
+        res.status(201).json({name: foundUser.name, id:foundUser.id})
+      } else {
+        res.status(401).json("OPSS")
+      }
+      return;
+    }
+  })
+  
+ 
 });
 
 module.exports = router;
